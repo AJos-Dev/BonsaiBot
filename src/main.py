452277655fe126b678f14f5ff5bb8e691f89472c
AJos-Dev <aayushjos@gmail.com>
2024-04-23 16:31:32 +0100

    fen board setting

--- src/main.py
+++ src/main.py
@@ -4,13 +4,32 @@ class piece_identifier(Enum):
     white = 16
     black = 8
     none = 0
-    king = 1
-    pawn = 2
-    knight = 3
-    bishop = 4
-    rook = 5
-    queen = 6
+    k = 1 # king
+    p = 2 # pawn
+    n = 3 # knight
+    b = 4 # bishop
+    r = 5 # rook
+    q = 6 # queen
 
-print(piece_identifier['pawn'].value)
+#turn pieces into letters for easier conversion with FEN
+board = [piece_identifier['none'].value] * 64
 
-#hi
+def set_board(FEN_board: str):
+    FEN_fields = FEN_board.split(" ")
+    gameboard_index = 0
+    for i in FEN_fields[0]:
+        print(i)
+        if ord(i) == 47: # a '/' in FEN must just be ignored
+            continue
+        elif ord(i) <= 57: # a number in FEN needs to jump that many spaces 
+            gameboard_index += int(i)
+        elif ord(i) <= 90: # a capital letter in FEN corresponds to a white piece
+            #board[gameboard_index] = piece_identifier[i.lower()].value |  piece_identifier['white'].value 
+            board[gameboard_index] = i
+        else: #vice versa for a black piece
+            #board[gameboard_index] = piece_identifier[i.lower()].value |  piece_identifier['black'].value
+            board[gameboard_index] = i
+
+
+set_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+print(board)
