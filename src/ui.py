fbb0ba7f8b795d54312e95f8c5c52792c1362c93
Aayush Joshi <aayushjos@gmail.com>
2024-11-19 17:19:52 +0000

    ui complete

--- src/ui.py
+++ src/ui.py
@@ -1,7 +1,7 @@
 import pygame
 import moves
 import random
-
+pygame.init()
 class Piece(pygame.sprite.Sprite):
     def __init__(self, square, board):
         super().__init__()
@@ -35,7 +35,7 @@ def createBoard():
     white = True
     for rank in range(8):
         for file in range(8):
-            tile = pygame.Rect(file * TILESIZE, rank * TILESIZE, TILESIZE, TILESIZE) #create a tile for every row and rank
+            tile = pygame.Rect(file * TILESIZE, rank * TILESIZE, TILESIZE, TILESIZE) #create a tile f or every row and rank
             pygame.draw.rect(chessboard_surf, pygame.Color('blanchedalmond' if white else 'burlywood4'), tile) #draw it, alternating black/white
             white= not white
         white= not white
@@ -68,6 +68,23 @@ def setPieces(board):
             piece = Piece(square_index, board)
             pieces_group.add(piece)
 
+button_font = pygame.font.Font(None, 25)
+class Button:
+    # Button class: initializes the button with position, text, font, color, and callback function
+    def __init__(self, text, position, size, piece):
+        self.rect = pygame.Rect(position, size)
+        self.text = text
+        self.piece = piece
+        self.fonts = button_font
+        self.color = (100, 100, 100)
+
+    def draw(self, surface):
+        # This draws the buttons onto the display
+        pygame.draw.rect(surface, self.color, self.rect)
+        text_surf = self.fonts.render(self.text, True, (255, 255, 255))
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        surface.blit(text_surf, text_rect)
+
 def main():
     moves_log=[]
     colour_to_play = 16
@@ -83,18 +100,37 @@ def main():
     drag = False
     setPieces(board)
     red_squares=[]
+    pawnPromotionMenu = False
     while True:
+        screen.fill(pygame.Color('black'))
         events = pygame.event.get()
+        if pawnPromotionMenu == True:
+            q_button = Button("Queen", (50, 10), (65, 55), moves.pieceIdentifier["q"].value)
+            n_button = Button("Knight", (50, 70), (65, 55), moves.pieceIdentifier["n"].value)
+            r_button = Button("Rook", (50, 130), (65, 55), moves.pieceIdentifier["r"].value)
+            b_button = Button("Bishop", (50, 190), (65, 55), moves.pieceIdentifier["b"].value)
+            q_button.draw(screen)
+            n_button.draw(screen)
+            r_button.draw(screen)
+            b_button.draw(screen)
+            buttons_list = [q_button, n_button, r_button, b_button]
         for event in events:
             if event.type == pygame.QUIT:
                 return
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 for piece in pieces_group:
-                    if piece.rect.collidepoint(event.pos):
+                    if piece.rect.collidepoint(event.pos) and not pawnPromotionMenu:
                         selected_piece = piece
                         old_square = selected_piece.getSquare()
                         drag = True
-            elif drag:
+                    elif pawnPromotionMenu:
+                        for button in buttons_list:
+                            if button.rect.collidepoint(event.pos):
+                                moves_log[-1].append(colour_to_play^24|button.piece)
+                                selected_piece.setPieceIdentifier(button.piece | colour_to_play^24)
+                                pawnPromotionMenu = False
+                                board[moves_log[-1][1]] = moves_log[-1][2]
+            elif drag and not pawnPromotionMenu:
                 for move in current_legal_moves:
                     if move[0] == selected_piece.getSquare():
                         red_squares.append(move[1])
@@ -106,7 +142,13 @@ def main():
                 if event.type == pygame.MOUSEBUTTONUP:
                     red_squares = []
                     move_to_play = [selected_piece.getSquare(), squareUnderMouse(white_perspective)[1]* 8 + squareUnderMouse(white_perspective)[0]]
-                    if move_to_play in current_legal_moves:
+
+                    #problem is currently when pawns promote to the final rank, the game registers that as [old square, new square] but they are stored in current legal moves as [old square, new square, new piece]
+                    if move_to_play in [i[:2] for i in current_legal_moves]:
+                        for move in current_legal_moves:
+                            if len(move) == 3 and move[:2] == move_to_play:
+                                pawnPromotionMenu = True
+                                break
                         #check whether the piece just moved was a king
                         if selected_piece.getPieceIdentifier()&7 == moves.pieceIdentifier["k"].value:
                             #if it is a king, did it move 2 squares (was it a castle)?
@@ -126,23 +168,24 @@ def main():
                                         piece.setPosition(position_lookup[move_to_play[1]+1])
                                         piece.setSquare(move_to_play[1]+1)
                                         moves_log.append([move_to_play[1]-2, move_to_play[1]+1])
+                        #remove the opposing piece in the target square
                         for piece in pieces_group:
                             if piece.getSquare() == move_to_play[1]:
                                 pieces_group.remove(piece)
+                        #update position on the board with the new move the pieces
                         selected_piece.setPosition(position_lookup[move_to_play[1]])
                         selected_piece.setSquare(move_to_play[1])
                         board[move_to_play[1]] = board[move_to_play[0]]
-                        board[move_to_play[0]] = moves.pieceIdentifier["none"].value
-                        moves_log.append(move_to_play)
-                        colour_to_play ^= 24
+                        board[move_to_play[0]] = moves.pieceIdentifier["none"].value#replace old square with an empty piece
+                        moves_log.append(move_to_play) #add move to move log
+                        colour_to_play ^= 24 #alternate between colours to play
                         current_legal_moves = moves.generateLegalMoves(colour_to_play, castling_rights, moves_log, board)
-                        if len(current_legal_moves) == 0:
+                        if len(current_legal_moves) == 0: #No moves to play indicates a checkmate
                             print("Checkmate", colour_to_play^24, "wins!")
                             exit()
-                    else:
+                    else:   
                         selected_piece.setPosition(position_lookup[old_square])
                     drag = False
-        screen.fill(pygame.Color('black'))
         screen.blit(chessboard_surf, OFFSET)
         pieces_group.draw(screen)
         for square in red_squares:
@@ -152,7 +195,7 @@ def main():
 
 pieces_group=pygame.sprite.Group()
 
-white_perspective = True
+white_perspective = False
 TILESIZE = 64
 OFFSET = (256, 128)
 
